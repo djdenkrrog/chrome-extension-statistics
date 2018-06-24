81c15e3f68d3ab3e32ca6c172b888643c3a89b75
makarov.denys <djdenkrrog1@rambler.ru>
2018-06-24 19:38:21 +0300

    Add output in console

--- background.js
+++ background.js
@@ -1,109 +1,57 @@
-var n = 0;
-// chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//     ++n;
-//     //console.log("onBeforeRequest details[" + n + "]: ", details);
-//     chrome.tabs.getSelected(function(tab) { //выбирается ид открытого таба, выполняется коллбек с ним
-//         chrome.tabs.sendRequest(tab.id, {details : details, n: n}); //запрос  на сообщение
-//     });
-// }, {
-//     urls : ["<all_urls>"]
-// }, ["requestBody"]);
-function printObject(obj, groupName) {
+var statisticParams = {};
 
-  chrome.tabs.executeScript({
-    code: 'console.groupCollapsed("' + (groupName ? groupName : 'Details') + '")'
-  });
-
-  for (var key in obj) {
-    if (key == 'requestBody') {
-      printObject(obj[key], key)
-    } else if (key == 'formData') {
-      printObject(obj[key], key)
-    } else {
-      chrome.tabs.executeScript({
-        code: 'console.log("' + key + ': ' + obj[key] + '")'
-      });
-    }
+function parsePostedString(string) {
+  var result = {};
+  var arrayParams = string.split('&');
+  for (var i = 0; i < arrayParams.length; i++) {
+    var tmp = arrayParams[i].split('=');
+    result[tmp[0]] = tmp[1];
   }
-  chrome.tabs.executeScript({
-    code: 'console.groupEnd()'
-  });
-
+  return result;
 }
 
-// chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//   ++n;
-//
-//   console.log('details: ', details);
-//   chrome.tabs.executeScript({
-//     code: 'console.log("printObject-background.js: ");'
-//   });
-//   //
-//   // chrome.tabs.executeScript({
-//   //   code: 'console.groupCollapsed("' + details.method + ' [' + n + '] ' + details.url + '");'
-//   // });
-//   //
-//   // if (details.requestBody && details.requestBody.formData) {
-//   //   chrome.tabs.executeScript({
-//   //     code: 'console.groupCollapsed("Params")'
-//   //   });
-//   //
-//   //   printObject(details.requestBody.formData, 'formData');
-//   //
-//   //   chrome.tabs.executeScript({
-//   //     code: 'console.groupEnd();'
-//   //   });
-//   // }
-//   //
-//   // printObject(details);
-//   //
-//   // chrome.tabs.executeScript({
-//   //   code: 'console.groupEnd()'
-//   // });
-// }, {
-//   urls: ["<all_urls>"]
-// }, ["requestBody"]);
-//
-// chrome.webRequest.onBeforeSendHeaders.addListener(
-//   function(details) {
-//     for (var i = 0; i < details.requestHeaders.length; ++i) {
-//       if (details.requestHeaders[i].name === 'User-Agent') {
-//         details.requestHeaders.splice(i, 1);
-//         break;
-//       }
-//     }
-//     return {requestHeaders: details.requestHeaders};
-//   },
-//   {urls: ["<all_urls>"]},
-//   ["blocking", "requestHeaders"]);
-
-// chrome.webRequest.onBeforeRequest.addListener(function(details) {
-//   chrome.tabs.executeScript({
-//     code: 'console.log("complited details")'
-//   });
-//   ///console.log('complited details', details);
-// }, {
-//   urls: ["<all_urls>"]
-// }, ["responseBody"]);
 
 /* The Web Request API */
 const WEB_REQUEST = chrome.webRequest;
 
-WEB_REQUEST.onBeforeRequest.addListener(
-  function(details) {
+WEB_REQUEST.onBeforeRequest.addListener(function(details) {
     var regExp = /^.*api_v3\/multi_export\/updateStatistic\/(\d+)/i;
-    var newDetails = Object.assign(details);
-    if (details.method == "POST")
+    if (details.method == "POST") {
       if (regExp.test(details.url)) {
-        console.log('URL_newDetails: ', newDetails);
-        console.log('URL: ', details);
-        var postedString = decodeURIComponent(String.fromCharCode.apply(null,
-          new Uint8Array(details.requestBody.raw[0].bytes)));
-        console.log('postedString: ', postedString);
         var statisticID = regExp.exec(details.url)[1];
+        statisticParams[statisticID] = {};
         console.log('statisticID: ', statisticID);
-        console.log('formData: ', details.requestBody.formData);
+        if (Array.isArray(details.requestBody.raw)) {
+          var postedString = decodeURIComponent(
+            String.fromCharCode.apply(null, new Uint8Array(details.requestBody.raw[0].bytes))
+          );
+          console.log('postedString: ', parsePostedString(postedString));
+          Object.assign(
+            statisticParams[statisticID],
+            parsePostedString(postedString)
+          );
+        }
+        //console.log('URL_newDetails: ', newDetails);
+        // console.log('URL: ', details);
+        //globalParams[statisticID] = [globalParams[statisticID],...details.requestBody.formData]
+        if (details.requestBody.formData) {
+          console.log('formData: ', details.requestBody.formData);
+          Object.assign(
+            statisticParams[statisticID],
+            details.requestBody.formData
+          );
+        }
+        console.log(statisticParams);
+        var str = "console.log('Statistic params: " + JSON.stringify(statisticParams) + "');";
+        console.log(str);
+        chrome.tabs.executeScript(
+          details.tabId,
+          {
+            code: "console.log('Statistic params: " + JSON.stringify(statisticParams) + "');"
+          }
+        );
       }
+    }
     //console.log('POST: ', JSON.stringify(details));
     //console.log('POST: ', details);
 
@@ -112,6 +60,7 @@ WEB_REQUEST.onBeforeRequest.addListener(
     // events[app_ready]:["6"]
     //type
     //console.log('POST: ', details.url);
+
   },
   { urls: ["<all_urls>"] },
   ["blocking", "requestBody"]
